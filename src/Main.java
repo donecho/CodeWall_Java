f923f5803d662a47ee0e4b82752f2f744259f990
ZinPhyoThant <zinphyothant060664@gmail.com>
2024-12-19 02:34:01 +0630

    Day 5

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,21 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //for loop for(initialize;condition;count){sout}
+        System.out.println("This is for loop");
+        for(int num1=10; num1<30; num1+=2){
+            System.out.print(num1+"\t");
+        }
 
+        System.out.println(" ");
+        //while loop
+        System.out.println("This is while loop");
+        int num2 = 10;
+        while(num2<20){
+            System.out.print("_"+num2);
+            num2++;
         }
     }
 }
