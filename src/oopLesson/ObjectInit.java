c22cefe2b9ee8787c71291fe0cfd298408ba3ad7
ZinPhyoThant <zinphyothant060664@gmail.com>
2025-01-13 03:17:08 +0630

    Day 6

--- src/oopLesson/ObjectInit.java
+++ src/oopLesson/ObjectInit.java
@@ -17,10 +17,28 @@ public class ObjectInit {
         Student std1 = new Student();
         std1.id = 2;
         std1.name = "Mg Thant";
+
+        //for anonymous object
+        //This Student 2 obj is not same.
+        System.out.println("For Anonymous Object - "+ new Student().id);
+        System.out.println("For Anonymous Object - "+ new Student().name);
+
+        Student student = new Student(),student1 = new Student();
+        System.out.println(student.id);
+        System.out.println(student1.name);
     }
 }
 
 //attributes => direct
 //method =>
 //constructor
+
+//new keyword
+//newInstance() method
+//clone()
+//deserialization
+//factory method
+
+// how to build object -> ClassName objName = new Con();
+// new Con() -> anonymous object
 
